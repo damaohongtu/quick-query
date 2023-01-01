3065195d4fc20cb724fd5692787db6ca31e3717f
damaohongtu <tianmao818@qq.com>
2023-01-01 15:23:45 +0800

    refactor: 关联图查询配置使用mybatis持久化

--- orderquery-demo/src/main/java/com/damaohongtu/orderquerydemo/OrderqueryDemoApplication.java
+++ orderquery-demo/src/main/java/com/damaohongtu/orderquerydemo/OrderqueryDemoApplication.java
@@ -3,7 +3,7 @@ package com.damaohongtu.orderquerydemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.damaohongtu"})
 public class OrderqueryDemoApplication {
 
     public static void main(String[] args) {
