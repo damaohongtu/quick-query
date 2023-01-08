49fa6e66fd50abfb169a350bf8b2116f2d074b8d
damaohongtu <tianmao818@qq.com>
2023-01-08 15:58:08 +0800

    feat:添加缩略图

--- quickquery-front/src/views/manage/orderquery/components/graph-component.vue
+++ quickquery-front/src/views/manage/orderquery/components/graph-component.vue
@@ -2,9 +2,10 @@
   <div class="app-container">
     <fieldset :disabled="isEditDisabled" style="border: none">
       <el-row>
-        <div class="graph">
+        <div class="graph" id="graph">
           <butterfly-vue
             :canvas-data="formattedData"
+            :base-canvas="baseCanvas"
           />
         </div>
       </el-row>
@@ -61,6 +62,7 @@
 
 <script>
 import { ButterflyVue } from 'butterfly-vue'
+import { Canvas } from 'butterfly-dag'
 import { PROCESSOR_DICT } from '../processor-dict.js'
 import { queryAllDataBase, queryAllHashInfo, queryGraphByCode } from '@/api/orderquery'
 
@@ -189,6 +191,64 @@ export default {
     })
   },
   methods: {
+    baseCanvas() {
+      const canvas = new Canvas({
+        // 如下属性
+        root: graph, // canvas的根节点(必传)
+        layout: { type: 'ForceLayout', options: {}}, // 布局设置(选填)，可使用集成的，也可自定义布局
+        layoutOptions: { rankdir: 'TB' }, // 布局配置(选填)，传入集成/自定义布局的参数
+        zoomable: true, // 可缩放(选填)
+        moveable: true, // 可平移(选填)
+        draggable: true, // 节点可拖动(选填)
+        linkable: true, // 节点可连接(选填)
+        disLinkable: true, // 节点可取消连接(选填)
+        theme: { // 主题定制(选填)
+          group: {
+            type: 'normal', // 节点组类型(选填): normal(随意拖入拖出),inner(只能拖入不能拖出)
+            dragGroupZIndex: 50 // 节点组的默认z-index（选填，默认值：50）
+          },
+          node: {
+            dragNodeZIndex: 250 // 节点的默认z-index/2（选填，默认值250）
+          },
+          edge: {
+            type: 'endpoint', // 线段连接类型
+            shapeType: 'Bezier', // 线条默认类型
+            hasRadius: false, // 默认曼哈顿曲线不为圆角
+            label: 'test', // 线条默认label
+            arrow: true, // 线条默认是否带箭头
+            arrowPosition: 0.5, // 箭头位置(0 ~ 1)
+            arrowOffset: 0.0, // 箭头偏移
+            arrowShapeType: '', // 自定义箭头样式
+            isExpandWidth: false, // 增加线条交互区域
+            defaultAnimate: false, // 默认开启线条动画
+            dragEdgeZindex: 499 // 线段的默认z-index(选填，默认：499)
+          },
+          endpoint: {
+            position: [], // 限制锚点位置['Top', 'Bottom', 'Left', 'Right'],
+            linkableHighlight: true, // 连线时会触发point.linkable的方法，可做高亮
+            limitNum: 10, // 限制锚点的连接数目
+            expandArea: { // 锚点过小时，可扩大连线热区
+              left: 10,
+              right: 10,
+              top: 10,
+              botton: 10
+            }
+          },
+          zoomGap: 0.001, // 鼠标放大缩小间隙设置
+          autoFixCanvas: { // 节点拖动或连线拖动到画布边缘时，画布自动延展
+            enable: false,
+            autoMovePadding: [20, 20, 20, 20] // 触发自动延展的画布内边距
+          },
+          autoResizeRootSize: true, // 自动适配root大小，默认为true
+          isMouseMoveStopPropagation: true // 拖动事件是否阻止冒泡
+        },
+        global: { // 自定义配置，会贯穿所有canvas，group，node，edge，endpoint对象
+          isScopeStrict: false // scope是否为严格模式(默认为false)
+        }
+      })
+      canvas.setMinimap(true, {/* options */})
+      return canvas
+    },
     disableEdit() {
       if (this.$route.name === 'QueryGraph') {
         this.isEditDisabled = true
