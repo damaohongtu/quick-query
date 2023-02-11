e4a4200601ec7bfdd4ead5e6737860129ed9432d
damaohongtu <tianmao818@qq.com>
2023-02-11 20:56:03 +0800

    feat: 添加数据源管理初始化

--- quickquery-spring-boot-starter/src/main/java/com/damaohongtu/quickquery/autoconfig/datasource/DataSourceDynamicConfig.java
+++ quickquery-spring-boot-starter/src/main/java/com/damaohongtu/quickquery/autoconfig/datasource/DataSourceDynamicConfig.java
@@ -1,11 +1,16 @@
 package com.damaohongtu.quickquery.autoconfig.datasource;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import com.damaohongtu.quickquery.dao.entity.Source;
 import com.damaohongtu.quickquery.dao.repo.SourceRepo;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+import ru.yandex.clickhouse.ClickHouseDataSource;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Component
@@ -15,10 +20,15 @@ public class DataSourceDynamicConfig {
     @Resource
     private SourceRepo sourceRepo;
 
-    private final Map<String, DataSource> mysqlDataSourceMap;
+    private Map<String, List<DruidDataSource>> dataSourceMap = new HashMap<>(8);
 
     public void buildDataSource(){
+        List<Source> sourceList = sourceRepo.list();
 
+    }
+
+    public void clear(){
+        dataSourceMap.clear();
     }
 
 }
--- quickquery-spring-boot-starter/src/main/java/com/damaohongtu/quickquery/dao/repo/SourceRepo.java
+++ quickquery-spring-boot-starter/src/main/java/com/damaohongtu/quickquery/dao/repo/SourceRepo.java
@@ -1,11 +1,24 @@
 package com.damaohongtu.quickquery.dao.repo;
 
+import com.damaohongtu.quickquery.dao.entity.Source;
+import com.damaohongtu.quickquery.dao.entity.SourceExample;
 import com.damaohongtu.quickquery.dao.mapper.SourceMapper;
+import com.damaohongtu.quickquery.util.Constants;
 import org.springframework.stereotype.Repository;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @Repository
 public class SourceRepo {
 
+    @Resource
     private SourceMapper sourceMapper;
+
+    public List<Source> list(){
+        SourceExample example = new SourceExample();
+        example.createCriteria().andValidEqualTo(Constants.VALID);
+        return sourceMapper.selectByExample(example);
+    }
 
 }
